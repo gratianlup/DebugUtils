95b6527a59d5afbea65c90131b93ddd5f32ae432
Gratian Lup <gratianlup@outlook.com>
2013-01-23 14:09:08 +0200

    Update DebugUtils/Listeners/TextFileListener.cs

--- DebugUtils/Listeners/TextFileListener.cs
+++ DebugUtils/Listeners/TextFileListener.cs
@@ -3,12 +3,20 @@
 // modification, are permitted provided that the following conditions are
 // met:
 //
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-//       copyright notice, this list of conditions and the following
-//       disclaimer in the documentation and/or other materials provided
-//       with the distribution.
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 
+// * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following
+// disclaimer in the documentation and/or other materials provided
+// with the distribution.
+//
+// * The name "DebugUtils" must not be used to endorse or promote 
+// products derived from this software without prior written permission.
+//
+// * Products derived from this software may not be called "DebugUtils" nor 
+// may "DebugUtils" appear in their names without prior written 
+// permission of the author.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -64,11 +72,9 @@ namespace DebugUtils.Debugger.Listeners {
 
             try {
                 writer = File.CreateText(_filePath);
-
                 writer.WriteLine("Text File listener opened at " + DateTime.Now.ToShortDateString().ToString() + " " + DateTime.Now.ToLongTimeString().ToString());
                 writer.WriteLine("**************************************************\n\n");
                 writer.Flush();
-
                 IsOpen = true;
             }
             catch(Exception e) {
@@ -78,7 +84,6 @@ namespace DebugUtils.Debugger.Listeners {
             }
 
             IsOpen = writer.BaseStream.CanWrite;
-
             return IsOpen;
         }
 
@@ -87,9 +92,7 @@ namespace DebugUtils.Debugger.Listeners {
                 writer.WriteLine("\n\n**************************************************");
                 writer.WriteLine("Text File listener closed at " + DateTime.Now.ToShortDateString().ToString() + " " + DateTime.Now.ToLongTimeString().ToString());
                 writer.Close();
-
                 IsOpen = false;
-
                 return true;
             }
 
@@ -104,27 +107,27 @@ namespace DebugUtils.Debugger.Listeners {
             writer.Write(message.Time.ToLongTimeString() + " | ");
             switch(message.Type) {
                 case DebugMessageType.Error: {
-                        writer.Write("Error");
-                        break;
-                    }
+                    writer.Write("Error");
+                    break;
+                }
                 case DebugMessageType.Warning: {
-                        writer.Write("Warning");
-                        break;
-                    }
+                    writer.Write("Warning");
+                    break;
+                }
                 default: {
-                        writer.Write("Unknown");
-                        break;
-                    }
+                    writer.Write("Unknown");
+                    break;
+                }
             }
 
             writer.WriteLine(" | #" + HandledMessages);
-
             writer.WriteLine(message.Message + "\n");
             writer.Write("METHOD: ");
             writer.WriteLine(GetMethodTypeString(message.BaseMethod.Type) + message.BaseMethod.Method);
             writer.Write("LINE:   ");
             writer.WriteLine(message.BaseMethod.Line);
             writer.Write("FILE:   ");
+            
             if(TruncateFile) {
                 FileInfo fi = new FileInfo(message.BaseMethod.File);
                 writer.WriteLine(fi.Name);
